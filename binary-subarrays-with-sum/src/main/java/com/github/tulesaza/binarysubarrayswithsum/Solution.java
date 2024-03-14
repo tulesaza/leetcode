6fb4a6decc6e3395aa89b31ff9c3a0dac03506a2
Azamat Tuleshov <tuleshov.azamat13@gmail.com>
2024-03-14 15:43:22 +0100

    Alternative solution for 930. Binary Subarrays With Sum

--- binary-subarrays-with-sum/src/main/java/com/github/tulesaza/binarysubarrayswithsum/Solution.java
+++ binary-subarrays-with-sum/src/main/java/com/github/tulesaza/binarysubarrayswithsum/Solution.java
@@ -4,6 +4,7 @@ class Solution {
 
     // 1. Bruteforce O(n^2)
     // 2. two sliding windows O(n)
+    // 3. Prefix sum counts
     public int numSubarraysWithSum(int[] nums, int goal) {
         int goalWindowLeft = 0, goalMinusOneWindowLeft = 0;
         int goalWindowSum = 0; // window where sum equals goal
@@ -22,5 +23,27 @@ class Solution {
             diffs += goalMinusOneWindowLeft - goalWindowLeft;
         }
         return diffs;
+    }
+
+    // Not mine, hard to understand
+    // [ 1 0 1 0 1 ] n = 5, max sum 5
+    //   0 1 2 3 4 5
+    //  [1 2 2 1 0 0 ]
+    //
+    public int numSubarraysWithSum1(int[] nums, int goal) {
+        int totalCount = 0;
+        int sum = 0;
+        int[] prefixSumCounts = new int[nums.length + 1];
+        prefixSumCounts[0] = 1;
+
+        for (int num : nums) {
+            sum += num;
+            if (sum >= goal) {
+                totalCount += prefixSumCounts[sum - goal];
+            }
+            prefixSumCounts[sum]++;
+        }
+
+        return totalCount;
     }
 }
